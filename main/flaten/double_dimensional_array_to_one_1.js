e3308d75c6294397533331970721d7e1a8ce28c9
Grace <lunay0325@gmail.com>
2018-12-13 23:17:28 +0800

    Complete all 'double dimensional array to one' practices and pass all tests

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function double_to_one(collection) {
+  let result = [];
 
-  //在这里写入代码
+  function flatArray(array) {
+    for (let item of array) {
+      result.push(item);
+    }
+  }
+  for (let item of collection) {
+    if (Array.isArray(item) === true) {
+      flatArray(item);
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
 }
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,26 @@
 'use strict';
 
 function double_to_one(collection) {
+  let result = [];
 
-  //在这里写入代码
+  function flatArray(array) {
+    for (let item of array) {
+      if (result.indexOf(item) < 0) {
+        result.push(item);
+      }
+    }
+  }
+
+  for (let item of collection) {
+    if (Array.isArray(item) === true) {
+      flatArray(item);
+    } else {
+      if (result.indexOf(item) < 0) {
+        result.push(item);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = double_to_one;
