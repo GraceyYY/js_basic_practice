eff8def81436a79bc5896527c844c622ec5f9dfb
Grace <lunay0325@gmail.com>
2018-12-13 23:31:59 +0800

    Complete the 'number map to word' functions and pass tests

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,10 @@
 'use strict';
-var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+
+var number_map_to_word = function(collection) {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  return collection.map((element) => {
+    return alphabet[element - 1];
+  })
 };
 
 module.exports = number_map_to_word;
--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,20 @@
 'use strict';
-var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+
+var number_map_to_word_over_26 = function(collection) {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  function getLetter(n) {
+    if (n <= 26) {
+      return alphabet[n - 1];
+    } else {
+      let firstLetter = alphabet[Math.floor((n - 1) / 26) - 1];
+      let secondLetter = alphabet[(n - 1) % 26];
+      return firstLetter + secondLetter;
+    }
+  }
+  return collection.map((element) => {
+    return getLetter(element);
+  })
 };
 
 module.exports = number_map_to_word_over_26;
