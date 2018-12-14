fa8255534c258761af03aea6d010bcc7a1aca7e2
Grace <lunay0325@gmail.com>
2018-12-15 00:16:27 +0800

    Complete the 'collect min number' function and pass the test

--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function collect_min_number(collection) {
-  //在这里写入代码
+  function rank_asc(array) {
+    for (let i = array.length; i >= 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (array[j] > array[j + 1]) {
+          let min = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = min;
+        }
+      }
+    }
+    return array;
+  }
+  return rank_asc(collection)[0];
 }
 
 module.exports = collect_min_number;
-
