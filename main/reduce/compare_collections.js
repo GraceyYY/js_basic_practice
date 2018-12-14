fde0e3376e2208a4efc9dffef8a5e93a0cc42ae8
Grace <lunay0325@gmail.com>
2018-12-15 01:31:03 +0800

    Complete the 'compare collections' function and pass the test

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -1,9 +1,20 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-  //在这里写入代码
+  if (collection_a.length > 0 && collection_b.length > 0) {
+    if (collection_a.length != collection_b.length) {
+      return false;
+    } else {
+      for (let i = 0; i < collection_a.length; i++) {
+        if (collection_a[i] != collection_b[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = compare_collections;
-
-
