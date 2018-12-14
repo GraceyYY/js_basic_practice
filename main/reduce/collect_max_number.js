165a472c0b791826879e0558e5753ef95e02ac93
Grace <lunay0325@gmail.com>
2018-12-15 00:10:47 +0800

    Complete the 'collect max number' function and pass the test

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function collect_max_number(collection) {
-  //在这里写入代码
+  function rank_desc(array) {
+    for (let i = array.length; i >= 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (array[j] < array[j + 1]) {
+          let min = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = min;
+        }
+      }
+    }
+    return array;
+  }
+  return rank_desc(collection)[0];
 }
 
 module.exports = collect_max_number;
