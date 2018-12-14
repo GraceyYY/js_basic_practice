c1714f8686c26c32978923b038a7c5cb61c48d9d
Grace <lunay0325@gmail.com>
2018-12-15 02:07:05 +0800

    Complete the 'compute median' function and pass the test

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -1,9 +1,29 @@
 'use strict';
 
 function compute_median(collection) {
-  //在这里写入代码
+
+  let nums = rank_asc(collection);
+  let result;
+
+  if (nums.length % 2 === 1) {
+    result = nums[Math.floor(nums.length / 2)]
+  } else {
+    result = (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2;
+  }
+  return result;
+
+  function rank_asc(array) {
+    for (let i = array.length; i >= 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (array[j] > array[j + 1]) {
+          let min = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = min;
+        }
+      }
+    }
+    return array;
+  }
 }
 
 module.exports = compute_median;
-
-
