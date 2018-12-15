361a127a3f69ae85f585167b5ebf01f969e5fd75
Grace <lunay0325@gmail.com>
2018-12-15 20:39:02 +0800

    Complete all practices in section1 and pass the tests

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,11 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  let flatArray = [];
+  for (let item of collection_b) {
+    flatArray = flatArray.concat(item);
+  }
+  return collection_a.filter((element) => {
+    return flatArray.indexOf(element) >= 0;
+  })
 }
 
 module.exports = collect_same_elements;
--- main/section_1/practice_4/practice.js
+++ main/section_1/practice_4/practice.js
@@ -1,5 +1,13 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  let resultArray = collection_a.filter((element) => {
+    return object_b.value.indexOf(element.key) >= 0;
+  })
+
+  let result = [];
+  for (let item of resultArray) {
+    result.push(item.key);
+  }
+  return result;
 }
 
 module.exports = collect_same_elements;
