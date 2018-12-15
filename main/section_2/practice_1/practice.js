0004f868d8691c43af9380c8d227b1c92684d51f
Grace <lunay0325@gmail.com>
2018-12-15 21:57:11 +0800

    Complete all 'count same elements' functions and pass the tests

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,22 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let count = {};
+  for (let item of collection) {
+    if (!count[item]) {
+      count[item] = 1;
+    } else {
+      count[item] += 1;
+    }
+  }
+
+  let result = [];
+  for (let attr in count) {
+    let temp = {
+      key: attr,
+      count: count[attr]
+    };
+    result.push(temp);
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
--- main/section_2/practice_3/practice.js
+++ main/section_2/practice_3/practice.js
@@ -1,5 +1,29 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let count = {};
+  collection.map((element) => {
+    let key, sum;
+    if (element.length > 1) {
+      key = element[0];
+      sum = parseInt(element.replace(/[^0-9]/ig, ""));
+    } else {
+      key = element;
+      sum = 1;
+    }
+    if (!count[key]) {
+      count[key] = sum;
+    } else {
+      count[key] += sum;
+    }
+  });
+  let result = [];
+  for (let attr in count) {
+    let temp = {
+      name: attr,
+      summary: count[attr]
+    };
+    result.push(temp);
+  }
+  return result;
 }
 
 module.exports = count_same_elements;
