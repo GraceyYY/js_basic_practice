9ea13931f0178227a382341175c0185db4ad33e0
Grace <lunay0325@gmail.com>
2018-12-15 14:27:33 +0800

    Complete the 'find first even' function and pass the test

--- main/reduce/find_first_even.js
+++ main/reduce/find_first_even.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function find_first_even(collection) {
-  //在这里写入代码
+  return collection.find((element) => {
+    return element % 2 === 0;
+  })
 }
 
 module.exports = find_first_even;
-
