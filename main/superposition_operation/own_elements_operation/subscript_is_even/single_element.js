af10c092659205cea721159a7475d4a99001809b
Grace <lunay0325@gmail.com>
2018-12-17 01:02:07 +0800

    Complete the 'single element' function and pass the test

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,14 @@
 'use strict';
-var single_element = function(collection){
-
+var single_element = function(collection) {
+  let even = collection.filter((element, index) => {
+    return index % 2 === 1;
+  });
+  let result = [];
+  for (let item of even) {
+    if (even.indexOf(item) === even.lastIndexOf(item)) {
+      result.push(item);
+    }
+  }
+  return result;
 };
 module.exports = single_element;
