2e3ce4529abbb69c3bc491079b1b0cb316405339
Grace <lunay0325@gmail.com>
2018-12-16 22:57:00 +0800

    Complete the 'hybrid operation' function and pass the test

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function hybrid_operation(collection) {
-
-  //在这里写入代码
+  return collection.reduce((accumulator, currentValue) => {
+    accumulator += (currentValue * 3 + 2);
+    return accumulator;
+  }, 0);
 }
-
 module.exports = hybrid_operation;
-
