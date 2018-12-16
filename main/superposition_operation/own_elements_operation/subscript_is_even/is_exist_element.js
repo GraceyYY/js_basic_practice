8a8ea01d22ce7f80eca47fca6c86a77c16550694
Grace <lunay0325@gmail.com>
2018-12-17 00:33:40 +0800

    Complete the 'is exist element' function and pass the test

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,8 @@
 'use strict';
-var is_exist_element = function(collection,element){
-
+var is_exist_element = function(collection, element) {
+  let evenIndex = collection.filter((element, index) => {
+    return index % 2 === 0;
+  });
+  return evenIndex.indexOf(element) >= 0;
 };
 module.exports = is_exist_element;
