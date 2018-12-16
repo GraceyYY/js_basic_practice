3633fd68571f6b0b3c674a9adf04076007a17dd9
Grace <lunay0325@gmail.com>
2018-12-17 01:40:04 +0800

    Fix the bug: wrong operation in return statement

--- main/reduce/find_last_even.js
+++ main/reduce/find_last_even.js
@@ -2,8 +2,7 @@
 
 function find_last_even(collection) {
   return collection.reverse().find((element) => {
-    return element & 2 === 0;
-  })
+    return element % 2 === 0;
+  });
 }
-
 module.exports = find_last_even;
