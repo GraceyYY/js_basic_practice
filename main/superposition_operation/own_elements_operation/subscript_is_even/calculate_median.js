2f41fc149828f5bcef6c47bfa6760bcfdb9b6688
Grace <lunay0325@gmail.com>
2018-12-16 23:44:41 +0800

    Complete the 'calculate median' function and pass the test

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,16 @@
 'use strict';
-var calculate_median = function(collection){
-
+var calculate_median = function(collection) {
+  function getMedian(collection) {
+    let index = Math.floor(collection.length / 2);;
+    if (collection.length % 2 === 1) {
+      return collection[index];
+    } else {
+      return Math.ceil((collection[index] + collection[index - 1]) / 2);
+    }
+  }
+  let even = collection.filter((element) => {
+    return element % 2 === 0;
+  });
+  return getMedian(even);
 };
 module.exports = calculate_median;
