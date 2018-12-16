81ed0b3d466da85625d95bbc42bc33102a8ec41e
Grace <lunay0325@gmail.com>
2018-12-16 22:35:37 +0800

    Complete the 'even to letter' function and pass the tests

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function even_to_letter(collection) {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  //在这里写入代码
+  function getLetter(n) {
+    if (n <= 26) {
+      return alphabet[n - 1];
+    } else {
+      let firstLetter = alphabet[Math.floor((n - 1) / 26) - 1];
+      let secondLetter = alphabet[(n - 1) % 26];
+      return firstLetter + secondLetter;
+    }
+  }
+  let result = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (i % 2 === 1) {
+      result.push(getLetter(collection[i]));
+    }
+  }
+  return result;
 }
-
 module.exports = even_to_letter;
