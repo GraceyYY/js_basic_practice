ddffdbb671662f57d00581dfed706b906a545498
Grace <lunay0325@gmail.com>
2018-12-16 23:02:42 +0800

    Complete the 'hybrid operation to uneven and calculate the sum' function and pass the test

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  let odd = collection.filter((element) => {
+    return element % 2 === 1;
+  });
+  return odd.reduce((accumulator, currentValue) => {
+    return accumulator + (currentValue * 3 + 5);
+  }, 0);
 }
-
 module.exports = hybrid_operation_to_uneven;
-
