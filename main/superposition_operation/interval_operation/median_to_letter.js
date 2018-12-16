6fdb30ce2bf7e3a8a1af6747697eb6b99a66d0eb
Grace <lunay0325@gmail.com>
2018-12-16 23:37:37 +0800

    Complete the 'calculate average' function and pass the test

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -2,6 +2,7 @@
 
 function median_to_letter(collection) {
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
   function getLetter(n) {
     if (n <= 26) {
       return alphabet[n - 1];
@@ -11,16 +12,15 @@ function median_to_letter(collection) {
       return firstLetter + secondLetter;
     }
   }
+
   function getMedian(collection) {
     let index = Math.floor(collection.length / 2);;
     if (collection.length % 2 === 1) {
-        return collection(index);
-    }else{
-        return Math.ceil((collection[index] + collection[index - 1]) / 2);
+      return collection(index);
+    } else {
+      return Math.ceil((collection[index] + collection[index - 1]) / 2);
     }
   }
-
   return getLetter(getMedian(collection));
 }
-
 module.exports = median_to_letter;
--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,13 @@
 'use strict';
-var calculate_average = function(collection){
-
+var calculate_average = function(collection) {
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < collection.length; i++) {
+    if (i % 2 === 1) {
+      sum += collection[i];
+      count += 1;
+    }
+  }
+  return Math.round(sum / count);
 };
 module.exports = calculate_average;
