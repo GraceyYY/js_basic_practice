b8c52ca1f0588ee28b09de5b1b37d97cbe8d8477
Grace <lunay0325@gmail.com>
2018-12-16 22:43:06 +0800

    Complete the 'average to letter' function and pass the test

--- main/superposition_operation/collection_operation/letter_mapping_2.js
+++ main/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function average_to_letter(collection) {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  //在这里写入代码
+  function getLetter(n) {
+    if (n <= 26) {
+      return alphabet[n - 1];
+    } else {
+      let firstLetter = alphabet[Math.floor((n - 1) / 26) - 1];
+      let secondLetter = alphabet[(n - 1) % 26];
+      return firstLetter + secondLetter;
+    }
+  }
+  let sum = collection.reduce((accumulator, currentValue) => {
+    return accumulator += currentValue;
+  });
+  let average = Math.ceil(sum / collection.length);
+  return getLetter(average);
 }
-
 module.exports = average_to_letter;
-
