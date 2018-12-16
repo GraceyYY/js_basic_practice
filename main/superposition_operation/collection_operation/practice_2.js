8ee2cc6a303db98aeaad8939b187c1fafe049fb7
Grace <lunay0325@gmail.com>
2018-12-16 22:59:47 +0800

    Complete the 'hybrid operation to uneven' function and pass the test

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
-  //在这里写入代码
+  let odd = collection.filter((element) => {
+    return element % 2 === 1;
+  });
+  return odd.map((element) => {
+    return element * 3 + 2;
+  })
 }
-
 module.exports = hybrid_operation_to_uneven;
-
