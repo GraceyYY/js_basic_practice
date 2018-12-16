47f7b9f6944a4f4dab968e712850c931c7e1e3f3
Grace <lunay0325@gmail.com>
2018-12-17 01:07:54 +0800

    Complete the 'even asc odd desc' function and pass the test

--- main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ main/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,37 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
+var even_asc_odd_desc = function(collection) {
+  let even = collection.filter((element) => {
+    return element % 2 === 0;
+  });
+  let odd = collection.filter((element) => {
+    return element % 2 === 1;
+  });
+  return rankDesc(even).concat(rankAsc(odd));
 
+  function rankDesc(collection) {
+    for (let i = collection.length; i >= 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (collection[j] > collection[j + 1]) {
+          let min = collection[j];
+          collection[j] = collection[j + 1];
+          collection[j + 1] = min;
+        }
+      }
+    }
+    return collection;
+  }
+
+  function rankAsc(collection) {
+    for (let i = collection.length; i >= 0; i--) {
+      for (let j = 0; j < i; j++) {
+        if (collection[j] < collection[j + 1]) {
+          let min = collection[j];
+          collection[j] = collection[j + 1];
+          collection[j + 1] = min;
+        }
+      }
+    }
+    return collection;
+  }
 };
 module.exports = even_asc_odd_desc;
