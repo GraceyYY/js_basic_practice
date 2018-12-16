36803fa46f75e0c565935dacb9ccc0f1d78121c6
Grace <lunay0325@gmail.com>
2018-12-16 23:09:09 +0800

    Complete the 'average uneven' function and pass the test

--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function average_uneven(collection) {
-
-  //在这里写入代码
+  let odd = collection.filter((element) => {
+    return element % 2 === 1;
+  });
+  let sum = odd.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  });
+  return Math.floor(sum / odd.length);
 }
-
 module.exports = average_uneven;
